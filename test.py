2f5f71dccfa5c9114772856e3487c56d2bf520a2
上野清雅 <uenoseiga@uenoseigas-MacBook-puro.local>
2020-07-09 18:54:58 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-"""
-このファイルに解答コードを書いてください
-"""
+res = []
+with open("input.txt", "r") as f:
+    for s_line in f:
+        res.append(s_line.strip("\n"))
+m = int(res.pop(-1))
+mydict = {}
+flag = False
+
+for s_line in res:
+    tmp = s_line.split(":")
+    i = int(tmp[0])
+    s = tmp[1]
+    if m % i == 0 :
+        mydict[i] = s
+        flag = True
+
+if __name__ == "__main__":
+    if flag:
+        for k, v in sorted(mydict.items()):
+            print(str(v), end='')
+    else:
+        print(m, end='')
+    print()
+
